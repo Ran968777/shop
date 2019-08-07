9262b6800d831001f2a2c85b29293d1cd077553b
ran968777 <17858274053@163.com>
2019-08-07 17:56:21 +0800

    first commit

--- src/main/java/com/aa/o2o/dto/JavaTest.java
+++
@@ -1,7 +0,0 @@
-package com.aa.o2o.dto;
-
-public class JavaTest {
-	public static void main(String[] args) {
-		System.err.println(111);
-	}
-}
--- src/main/java/com/aa/o2o/service/AreaService.java
+++
@@ -1,9 +0,0 @@
-package com.aa.o2o.service;
-
-import java.util.List;
-
-import com.aa.o2o.entity.Area;
-
-public interface AreaService {
-	List<Area> getAreaList();
-}
